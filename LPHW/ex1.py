2deecc972f77ea58b170b53a49b4aa619027a16a
dogzz <afon@ukr.net>
2012-12-26 01:34:23 +0200

    try to print

--- LPHW/ex1.py
+++ LPHW/ex1.py
@@ -1,4 +1,10 @@
 #encoding=utf-8
 
 if __name__ == '__main__':
-    pass
+    print "Hello World!"
+    print "Hello Again"
+    print "I like typing this."
+    print "This is fun."
+    print 'Yay! Printing.'
+    print "I'd much rather you 'not'."
+    print 'I "said" do not touch this.'
