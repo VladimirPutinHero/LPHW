5c351ebafbec2bc07783037669d74630fcc2fbbe
dogzz <afon@ukr.net>
2012-12-27 12:40:39 +0200

    ввв

--- LPHW/ex1.py
+++ LPHW/ex1.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     print 'I "said" do not touch this.'
     print "123444567999aaaaaaaaaaaa123"
     print "русский текст"
-    print "русс эгейн"
+    print "русс эгейн1111"
 #C:\Programs\workspace\.metadata\.plugins\com.aptana.portablegit.win32\bin\git.exe
 #this is Git executable
 #Checkbox "Push" in commit dialog
