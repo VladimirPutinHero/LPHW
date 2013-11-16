7df33b1d69ed67f9306763cbab77cdeb40901d01
dogzz <afon@ukr.net>
2013-11-16 22:39:20 +0200

    ddsd

--- LPHW/ex40.py
+++ LPHW/ex40.py
@@ -1,3 +1,121 @@
 #encoding=utf-8
 
-print "хеллоу ворд"
+
+#import ex1
+
+#ex1.apple()
+
+class MyStuff(object):
+    def __init__(self):
+        self.tangerine = "And now and thousand years between"
+
+    def apple(self):
+        print "I AM Glassy Apples!"
+
+a = ["line1", "line2", "line3"]
+b = ["parade1", "parade2", "parade3"]
+
+
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+happy_bday = Song(a)
+
+bulls_on_parade = Song(b)
+
+happy_bday.sing_me_a_song()
+bulls_on_parade.sing_me_a_song()
+
+
+thing = MyStuff()
+thing.apple()
+print thing.tangerine
+
+
+import random
+from urllib import urlopen
+import sys
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+    "class %%%(%%%):":
+      "Make a class named %%% that is-a %%%.",
+    "class %%%(object):\n\tdef __init__(self, ***)":
+      "class %%% has-a __init__ that takes self and *** parameters.",
+    "class %%%(object):\n\tdef ***(self, @@@)":
+      "class %%% has-a function named *** that takes self and @@@ parameters.",
+    "*** = %%%()":
+      "Set *** to an instance of class %%%.",
+    "***.***(@@@)":
+      "From *** get the *** function, and call it with parameters self, @@@.",
+    "***.*** = '***'":
+      "From *** get the *** attribute and set it to '***'."
+}
+
+# do they want to drill phrases first
+PHRASE_FIRST = True
+if len(sys.argv) == 2 and sys.argv[1] == "english":
+    PHRASE_FIRST = True
+
+# load up the words from the website
+for word in urlopen(WORD_URL).readlines():
+    WORDS.append(word.strip())
+
+
+def convert(snippet, phrase):
+    class_names = [w.capitalize() for w in
+                   random.sample(WORDS, snippet.count("%%%"))]
+    other_names = random.sample(WORDS, snippet.count("***"))
+    results = []
+    param_names = []
+
+    for i in range(0, snippet.count("@@@")):
+        param_count = random.randint(1, 3)
+        param_names.append(', '.join(random.sample(WORDS, param_count)))
+
+    for sentence in snippet, phrase:
+        result = sentence[:]
+
+        # fake class names
+        for word in class_names:
+            result = result.replace("%%%", word, 1)
+
+        # fake other names
+        for word in other_names:
+            result = result.replace("***", word, 1)
+
+        # fake parameter lists
+        for word in param_names:
+            result = result.replace("@@@", word, 1)
+
+        results.append(result)
+
+    return results
+
+
+# keep going until they hit CTRL-D
+try:
+    while True:
+        snippets = PHRASES.keys()
+        random.shuffle(snippets)
+
+        for snippet in snippets:
+            phrase = PHRASES[snippet]
+            question, answer = convert(snippet, phrase)
+            if PHRASE_FIRST:
+                question, answer = answer, question
+
+            print question
+
+            raw_input("> ")
+            print "ANSWER:  %s\n\n" % answer
+except EOFError:
+    print "\nBye"
--- LPHW/ex43.py
+++ LPHW/ex43.py
@@ -7,7 +7,8 @@ from random import randint
 class Scene(object):
 
     def enter(self):
-        print "This scene is not yet configured. Subclass it and implement enter()."
+        print """This scene is not yet configured.
+        Subclass it and implement enter()."""
         exit(1)
 
 
